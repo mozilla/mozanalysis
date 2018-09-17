45046d9110bcd787666f81e334a4226872f6ee0f
Rob Hudson <robhudson@mozilla.com>
2018-09-17 16:33:21 -0700

    Implement distributed bootstrap (bug 1485811)

---
+++ tests/conftest.py
@@ -0,0 +1,29 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+import logging
+
+import pytest
+from pyspark.sql import SparkSession
+
+
+@pytest.fixture(scope="session")
+def spark():
+    spark = (
+        SparkSession
+        .builder
+        .master("local")
+        .appName("mozanalysis_test")
+        .getOrCreate()
+    )
+
+    logger = logging.getLogger("py4j")
+    logger.setLevel(logging.ERROR)
+
+    yield spark
+    spark.stop()
+
+
+@pytest.fixture(scope="session")
+def spark_context(spark):
+    return spark.sparkContext
