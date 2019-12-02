a3a0f261f71908e1dd5033cdcf6d0678e4ca76a2
Corey Dow-Hygelund <cdowhygelund@mozilla.com>
2019-12-02 13:52:11 -0800

    Adding simple import test for `metrics.fenix`.

--- tests/test_metric_libraries.py
+++ tests/test_metric_libraries.py
@@ -1,7 +1,9 @@
 import mozanalysis.metrics.desktop as mmd
 import mozanalysis.metrics.firetv as mmfftv
+import mozanalysis.metrics.fenix as mmf
 
 
 def test_imported_ok():
     assert mmd.active_hours
     assert mmfftv.home_tile_clicks
+    assert mmf.duration
