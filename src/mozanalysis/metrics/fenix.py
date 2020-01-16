7cd77959bbf4dd30aa383ac2bd30cf4a219eeb0c
Felix Lawrence <flawrence@mozilla.com>
2020-01-15 18:06:33 -0800

    client_info again

--- src/mozanalysis/metrics/fenix.py
+++ src/mozanalysis/metrics/fenix.py
@@ -78,13 +78,13 @@ user_reload_count = Metric(
 baseline_ping_count = Metric(
     name='baseline_ping_count',
     data_source=baseline,
-    select_expr='COUNT(ping_info.client_id)'
+    select_expr='COUNT(client_info.client_id)'
 )
 
 metric_ping_count = Metric(
     name='metric_ping_count',
     data_source=metrics,
-    select_expr='COUNT(ping_info.client_id)'
+    select_expr='COUNT(client_info.client_id)'
 )
 
 first_run_date = Metric(
