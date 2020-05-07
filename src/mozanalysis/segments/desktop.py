c47dc0c420d453a4dd9739e516237e28cb8c366c
Felix Lawrence <flawrence@mozilla.com>
2020-05-07 13:51:46 -0700

    Update usage regularity segments to v3.
    Docs for these segments are [on DTMO](https://docs.telemetry.mozilla.org/concepts/segments.html#regular-users-v3).

--- src/mozanalysis/segments/desktop.py
+++ src/mozanalysis/segments/desktop.py
@@ -13,64 +13,18 @@ clients_last_seen_lag_1_day = SegmentDataSource(
 )
 
 
-# Before https://github.com/mozilla/bigquery-etl/pull/825/ is merged,
-# we gotta do our own transforms
-tmp_segment_regular_users_v2 = """  CASE
-  WHEN
-    BIT_COUNT(days_visited_5_uri_bits & 0x0FFFFFFE) = 0
-  THEN
-    'new_irregular_users_v2'
-  WHEN
-    BIT_COUNT(days_visited_5_uri_bits & 0x0FFFFFFE)
-    BETWEEN 1
-    AND 7
-  THEN
-    'semi_regular_users_v2'
-  WHEN
-    BIT_COUNT(days_visited_5_uri_bits & 0x0FFFFFFE)
-    BETWEEN 8
-    AND 27
-  THEN
-    'regular_users_v2'
-  END
-"""
-
-
-regular_users_v2 = Segment(
-    name='regular_users_v2',
+regular_users_v3 = Segment(
+    name='regular_users_v3',
     data_source=clients_last_seen_lag_1_day,
-    # select_expr="""MAX(
-    #     COALESCE(segment_regular_users_v2, 'new_irregular_users_v2')
-    #     = 'regular_users_v2'
-    # )""",
     select_expr=f"""MAX(
-        COALESCE({tmp_segment_regular_users_v2}, 'new_irregular_users_v2')
-        = 'regular_users_v2'
+        BIT_COUNT(COALESCE(days_seen_bits, 0) & 0x0FFFFFFE) >= 14
     )""",
 )
 
-semi_regular_users_v2 = Segment(
-    name='semi_regular_users_v2',
+new_or_resurrected_v3 = Segment(
+    name='new_or_resurrected_v3',
     data_source=clients_last_seen_lag_1_day,
-    # select_expr="""MAX(
-    #     COALESCE(segment_regular_users_v2, 'new_irregular_users_v2')
-    #     = 'semi_regular_users_v2'
-    # )""",
     select_expr=f"""MAX(
-        COALESCE({tmp_segment_regular_users_v2}, 'new_irregular_users_v2')
-        = 'semi_regular_users_v2'
-    )""",
-)
-
-new_irregular_users_v2 = Segment(
-    name='new_irregular_users_v2',
-    data_source=clients_last_seen_lag_1_day,
-    # select_expr="""MAX(
-    #     COALESCE(segment_regular_users_v2, 'new_irregular_users_v2')
-    #     = 'new_irregular_users_v2'
-    # )""",
-    select_expr=f"""MAX(
-        COALESCE({tmp_segment_regular_users_v2}, 'new_irregular_users_v2')
-        = 'new_irregular_users_v2'
+        BIT_COUNT(COALESCE(days_seen_bits, 0) & 0x0FFFFFFE) = 0
     )""",
 )
