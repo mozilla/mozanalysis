mozilla/mozanalysis

commit edc44eb2c28e4b124bac0dd3881b436903900b16

Use simple queries for enrollments and metrics SQL

src/mozanalysis/bq.py
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import re
 
-from google.api_core.exceptions import NotFound
+from google.api_core.exceptions import Conflict
 from google.cloud import bigquery
 
 
@@ -36,31 +36,52 @@ class BigQueryContext:
         self.project_id = project_id
         self.client = bigquery.Client(project=project_id)
 
-    def run_script_or_fetch(self, sql, results_table):
-        """Runs a BigQuery SQL script and returns a RowIterator for results_table.
-        The script is assumed to create a table named results_table after completing
-        succesfully. If results_table already exists, a RowIterator for the
-        existing table will be returned without invoking the script.
-
-        results_table is assumed to be an unqualified table name without
-        a project or dataset reference.
-
-        Learn more about BigQuery scripting at
-        https://cloud.google.com/bigquery/docs/reference/standard-sql/scripting.
+    def run_query(self, sql, results_table=None) -> str:
+        """Runs a SQL query and returns the destination table.
+        If ``results_table`` is provided, then save the results
+        into there otherwise save results in a temporary table.
         """
+        if results_table:
+            job = self.client.query(
+                sql,
+                job_config=bigquery.QueryJobConfig(
+                    destination=self.client.dataset(self.dataset_id).table(
+                        results_table
+                    )
+                ),
+            )
+        else:
+            job = self.client.query(sql)
 
-        fqtn = self.fully_qualify_table_name(results_table)
+        job.result()
+        return job.destination
+
+    def run_query_and_fetch(self, sql, results_table=None):
+        """Run a query and return the result.
+        If ``results_table`` is provided, then save the results
+        into there (or just query from there if it already exists).
+        Returns a ``google.cloud.bigquery.table.RowIterator``
+        """
+        if not results_table:
+            return self.client.query(sql).result()
 
         try:
-            cached = self.client.list_rows(fqtn)
-            print("Full results table already exists. Reusing", results_table)
-            return cached
-        except NotFound:
-            pass
+            full_res = self.client.query(
+                sql,
+                job_config=bigquery.QueryJobConfig(
+                    destination=self.client.dataset(self.dataset_id).table(
+                        results_table
+                    )
+                ),
+            ).result()
+            print("Saved into", results_table)
+            return full_res
 
-        self.client.query(sql).result()
-        print("Saved into", results_table)
-        return self.client.list_rows(fqtn)
+        except Conflict:
+            print("Full results table already exists. Reusing", results_table)
+            return self.client.query(
+                "SELECT * FROM {}".format(self.fully_qualify_table_name(results_table))
+            ).result()
 
     def fully_qualify_table_name(self, table_name):
         """Given a table name, return it fully qualified."""

src/mozanalysis/experiment.py
@@ -184,23 +184,35 @@ class Experiment:
             self.num_dates_enrollment,
         )
 
-        sql_template = self.build_query(
-            metric_list=metric_list,
+        enrollments_sql = self.build_enrollments_query(
             time_limits=time_limits,
             enrollments_query_type=enrollments_query_type,
             custom_enrollments_query=custom_enrollments_query,
             segment_list=segment_list,
         )
 
+<<<<<<< HEAD
         full_res_table_name = sanitize_table_name_for_bq(
             "_".join(
                 [last_date_full_data, self.experiment_slug, hash_ish(sql_template)]
             )
+=======
+        enrollments_table = bq_context.run_query(enrollments_sql)
+
+        metrics_sql = self.build_metrics_query(
+            metric_list=metric_list,
+            time_limits=time_limits,
+            enrollments_table=enrollments_table,
+>>>>>>> Use simple queries for enrollments and metrics SQL
         )
 
-        sql = sql_template.format(results_table=full_res_table_name)
+        full_res_table_name = sanitize_table_name_for_bq(
+            "_".join([last_date_full_data, self.experiment_slug, hash_ish(metrics_sql)])
+        )
 
-        return bq_context.run_script_or_fetch(sql, full_res_table_name).to_dataframe()
+        return bq_context.run_query_and_fetch(
+            metrics_sql, full_res_table_name
+        ).to_dataframe()
 
     def get_time_series_data(
         self,
@@ -271,23 +283,33 @@ class Experiment:
             self.num_dates_enrollment,
         )
 
-        sql_template = self.build_query_template(
-            metric_list=metric_list,
+        enrollments_sql = self.build_enrollments_query(
             time_limits=time_limits,
             enrollments_query_type=enrollments_query_type,
             custom_enrollments_query=custom_enrollments_query,
             segment_list=segment_list,
         )
 
+<<<<<<< HEAD
         full_res_table_name = sanitize_table_name_for_bq(
             "_".join(
                 [last_date_full_data, self.experiment_slug, hash_ish(sql_template)]
             )
+=======
+        enrollments_table = bq_context.run_query(enrollments_sql)
+
+        metrics_sql = self.build_metrics_query(
+            metric_list=metric_list,
+            time_limits=time_limits,
+            enrollments_table=enrollments_table,
+>>>>>>> Use simple queries for enrollments and metrics SQL
         )
 
-        sql = sql_template.format(results_table=full_res_table_name)
+        full_res_table_name = sanitize_table_name_for_bq(
+            "_".join([last_date_full_data, self.experiment_slug, hash_ish(metrics_sql)])
+        )
 
-        bq_context.run_script_or_fetch(sql, full_res_table_name)
+        bq_context.run_query_and_fetch(metrics_sql, full_res_table_name)
 
         return TimeSeriesResult(
             fully_qualified_table_name=bq_context.fully_qualify_table_name(
@@ -296,18 +318,14 @@ class Experiment:
             analysis_windows=time_limits.analysis_windows,
         )
 
-    def build_query_template(
+    def build_enrollments_query(
         self,
-        metric_list,
         time_limits,
         enrollments_query_type="normandy",
         custom_enrollments_query=None,
         segment_list=None,
     ) -> str:
-        """Return a SQL template for querying metric data.
-
-        The return value is a Python string template that needs to be formatted
-        with a value for `results_table` before it is valid SQL.
+        """Return a SQL query for querying enrollments data.
 
         For interactive use, prefer :meth:`.get_time_series_data` or
         :meth:`.get_single_window_data`, according to your use case,
@@ -315,11 +333,8 @@ class Experiment:
         dataframe.
 
         Args:
-            metric_list (list of mozanalysis.metric.Metric):
-                The metrics to analyze.
             time_limits (TimeLimits): An object describing the
                 interval(s) to query
-            results_table (str): The name of the table to write
             enrollments_query_type ('normandy' or 'fenix-fallback'):
                 Specifies the query type to use to get the experiment's
                 enrollments, unless overridden by
@@ -343,42 +358,82 @@ class Experiment:
 
         enrollments_query = custom_enrollments_query or self._build_enrollments_query(
             time_limits, enrollments_query_type
+<<<<<<< HEAD
         )
 
         metrics_columns, metrics_joins = self._build_metrics_query_bits(
             metric_list, time_limits
+=======
+>>>>>>> Use simple queries for enrollments and metrics SQL
         )
 
         segments_query = self._build_segments_query(segment_list, time_limits)
 
         return """
-    CREATE TEMPORARY TABLE enrollments AS (
-        WITH analysis_windows AS (
-            {analysis_windows_query}
-        ),
-        raw_enrollments AS ({enrollments_query}),
-        segmented_enrollments AS ({segments_query})
+            WITH analysis_windows AS (
+                {analysis_windows_query}
+            ),
+            raw_enrollments AS ({enrollments_query}),
+            segmented_enrollments AS ({segments_query})
 
-        SELECT
-            e.*,
-            aw.*
-        FROM segmented_enrollments e
-        CROSS JOIN analysis_windows aw
-    );
-
-    CREATE OR REPLACE TABLE {{results_table}} AS (
-        SELECT
-            enrollments.*,
-            {metrics_columns}
-        FROM enrollments
-        {metrics_joins}
-    );
+            SELECT
+                e.*,
+                aw.*
+            FROM segmented_enrollments e
+            CROSS JOIN analysis_windows aw
         """.format(
+            analysis_windows_query=analysis_windows_query,
+            enrollments_query=enrollments_query,
+            segments_query=segments_query,
+        )
+
+    def build_metrics_query(
+        self,
+        metric_list,
+        time_limits,
+        enrollments_table,
+    ) -> str:
+        """Return a SQL query for querying metric data.
+
+        For interactive use, prefer :meth:`.get_time_series_data` or
+        :meth:`.get_single_window_data`, according to your use case,
+        which will run the query for you and return a materialized
+        dataframe.
+
+        Args:
+            metric_list (list of mozanalysis.metric.Metric):
+                The metrics to analyze.
+            time_limits (TimeLimits): An object describing the
+                interval(s) to query
+            enrollments_table (str): The name of the enrollments table
+
+        Returns:
+            A string containing a BigQuery SQL expression.
+
+        Building this query is the main goal of this module.
+        """
+        metrics_columns, metrics_joins = self._build_metrics_query_bits(
+            metric_list, time_limits
+        )
+
+        return """
+        SELECT
+            {enrollments_table}.*,
+            {metrics_columns}
+        FROM {enrollments_table}
+        {metrics_joins}
+        """.format(
+<<<<<<< HEAD
             analysis_windows_query=analysis_windows_query,
             enrollments_query=enrollments_query,
             segments_query=segments_query,
             metrics_columns=",\n        ".join(metrics_columns),
             metrics_joins="\n".join(metrics_joins),
+=======
+            metrics_columns=",\n        ".join(metrics_columns),
+            metrics_joins="\n".join(metrics_joins),
+            enrollments_table=enrollments_table,
+>>>>>>> Use simple queries for enrollments and metrics SQL
         )
 
     @staticmethod
@@ -392,7 +447,11 @@ class Experiment:
         This method writes the SQL to define the analysis window table.
         """
         return "\n        UNION ALL\n        ".join(
+<<<<<<< HEAD
             "(SELECT {aws} AS analysis_window_start, {awe} AS analysis_window_end)".format(  # noqa:E501
+=======
+            "(SELECT {aws} AS analysis_window_start, {awe} AS analysis_window_end)".format(
+>>>>>>> Use simple queries for enrollments and metrics SQL
                 aws=aw.start,
                 awe=aw.end,
             )
@@ -841,7 +900,7 @@ class TimeSeriesResult:
                     "AnalysisWindow not found with start of {}".format(analysis_window)
                 )
 
-        return bq_context.run_query(
+        return bq_context.run_query_and_fetch(
             self._build_analysis_window_subset_query(analysis_window)
         ).to_dataframe()
 

src/mozanalysis/metrics/desktop.py
@@ -142,8 +142,8 @@ search_count = Metric(
         Search Access Points.
         Learn more in the
         [search data documentation](https://docs.telemetry.mozilla.org/datasets/search.html).
-    """  # noqa:E501
-    ),
+    """
+    ),  # noqa:E501
 )
 
 #: Metric: ...
@@ -159,8 +159,8 @@ tagged_search_count = Metric(
         and were potentially revenue-generating.
         Learn more in the
         [search data documentation](https://docs.telemetry.mozilla.org/datasets/search.html).
-    """  # noqa:E501
-    ),
+    """
+    ),  # noqa:E501
 )
 
 #: Metric: ...
@@ -176,8 +176,8 @@ tagged_follow_on_search_count = Metric(
         results page after executing a tagged search through a SAP.
         Learn more in the
         [search data documentation](https://docs.telemetry.mozilla.org/datasets/search.html).
-    """  # noqa:E501
-    ),
+    """
+    ),  # noqa:E501
 )
 
 #: Metric: ...
@@ -206,8 +206,8 @@ searches_with_ads = Metric(
         Users may not actually see these ads thanks to e.g. ad-blockers.
         Learn more in the
         [search analysis documentation](https://mozilla-private.report/search-analysis-docs/book/in_content_searches.html).
-    """  # noqa:E501
-    ),
+    """
+    ),  # noqa:E501
 )
 
 #: Metric: ...
@@ -222,8 +222,8 @@ organic_search_count = Metric(
         through a Firefox SAP and which are not monetizable.
         Learn more in the
         [search data documentation](https://docs.telemetry.mozilla.org/datasets/search.html).
-    """   # noqa:E501
-    ),
+    """
+    ),  # noqa:E501
 )
 
 #: Metric: ...

tests/test_experiment.py
@@ -244,13 +244,19 @@ def test_query_not_detectably_malformed():
         num_dates_enrollment=8,
     )
 
-    sql = exp.build_query_template(
+    enrollments_sql = exp.build_enrollments_query(
+        time_limits=tl, enrollments_query_type="normandy"
+    )
+
+    sql_lint(enrollments_sql)
+
+    metrics_sql = exp.build_metrics_query(
         metric_list=[],
         time_limits=tl,
-        enrollments_query_type="normandy",
-    ).format(results_table="foo")
+        enrollments_table="enrollments",
+    )
 
-    sql_lint(sql)
+    sql_lint(metrics_sql)
 
 
 def test_megaquery_not_detectably_malformed():
@@ -263,13 +269,19 @@ def test_megaquery_not_detectably_malformed():
         num_dates_enrollment=8,
     )
 
-    sql = exp.build_query_template(
+    enrollments_sql = exp.build_enrollments_query(
+        time_limits=tl, enrollments_query_type="normandy"
+    )
+
+    sql_lint(enrollments_sql)
+
+    metrics_sql = exp.build_metrics_query(
         metric_list=[m for m in mad.__dict__.values() if isinstance(m, mad.Metric)],
         time_limits=tl,
-        enrollments_query_type="normandy",
-    ).format(results_table="foo")
+        enrollments_table="enrollments",
+    )
 
-    sql_lint(sql)
+    sql_lint(metrics_sql)
 
 
 def test_segments_megaquery_not_detectably_malformed():
@@ -282,14 +294,21 @@ def test_segments_megaquery_not_detectably_malformed():
         num_dates_enrollment=8,
     )
 
-    sql = exp.build_query_template(
-        metric_list=[m for m in mad.__dict__.values() if isinstance(m, mad.Metric)],
-        segment_list=[s for s in msd.__dict__.values() if isinstance(s, msd.Segment)],
+    enrollments_sql = exp.build_enrollments_query(
         time_limits=tl,
+        segment_list=[s for s in msd.__dict__.values() if isinstance(s, msd.Segment)],
         enrollments_query_type="normandy",
-    ).format(results_table="foo")
+    )
 
-    sql_lint(sql)
+    sql_lint(enrollments_sql)
+
+    metrics_sql = exp.build_metrics_query(
+        metric_list=[m for m in mad.__dict__.values() if isinstance(m, mad.Metric)],
+        time_limits=tl,
+        enrollments_table="enrollments",
+    )
+
+    sql_lint(metrics_sql)
 
 
 def test_app_id_propagates():
@@ -314,21 +333,28 @@ def test_app_id_propagates():
         data_source=sds,
     )
 
-    sql = exp.build_query_template(
+    enrollments_sql = exp.build_enrollments_query(
+        time_limits=tl, segment_list=[segment], enrollments_query_type="fenix-fallback"
+    )
+
+    sql_lint(enrollments_sql)
+
+    metrics_sql = exp.build_metrics_query(
         metric_list=[
             m
             for m in mozanalysis.metrics.fenix.__dict__.values()
             if isinstance(m, Metric)
         ],
-        segment_list=[segment],
         time_limits=tl,
-        enrollments_query_type="fenix-fallback",
-    ).format(results_table="foo")
+        enrollments_table="enrollments",
+    )
 
-    assert "org_mozilla_firefox" not in sql
-    assert "my_cool_app" in sql
+    sql_lint(metrics_sql)
 
-    sql_lint(sql)
+    assert "org_mozilla_firefox" not in enrollments_sql
+    assert "my_cool_app" in enrollments_sql
+
+    sql_lint(metrics_sql)
 
 
 def test_query_not_detectably_malformed_fenix_fallback():
@@ -341,10 +367,16 @@ def test_query_not_detectably_malformed_fenix_fallback():
         num_dates_enrollment=8,
     )
 
-    sql = exp.build_query_template(
+    enrollments_sql = exp.build_enrollments_query(
+        time_limits=tl, enrollments_query_type="fenix-fallback"
+    )
+
+    sql_lint(enrollments_sql)
+
+    metrics_sql = exp.build_metrics_query(
         metric_list=[],
         time_limits=tl,
-        enrollments_query_type="fenix-fallback",
-    ).format(results_table="foo")
+        enrollments_table="enrollments",
+    )
 
-    sql_lint(sql)
+    sql_lint(metrics_sql)

commit 3614a8ddbba078b6942fa01d3635db568cd542e5

Join enrollments more rigorously... and fix what was broken (#120)
* Fixes #117. Handle duplicate client_ids more correctly.

Metric DataSources were not specific-enough when joining to the `enrollments` view: when there are multiple rows for one client_id, with different branches, rows for all branches are used for all the enrollment rows. So a cloned profile in a three-branch experiment could have up to 3*7=21 days of use in a 7 day analysis window.

Really we want to `GROUP BY e.*` when aggregating over the data source, but being SQL we need to manually list the full set of columns that uniquely identify a row.

Then, when joining this result to the others in the relevant `Experiment` method, we need to use all these join keys again.

* Handle duplicate client_ids more correctly.

Segment DataSources were not specific-enough when joining to the `enrollments` view: when there are multiple enrollments for one client_id (with different branches), the join won't be a one-to-many join from one enrollment to potentially many data source rows, it will be a many-to-many join, yielding potentially incorrect results due to the extra/duplicated datasource rows. If the enrollments have different enrollment_dates then there's further potential for error.

Really, we want to `GROUP BY e.*` when aggregating over the data source, but being SQL we need to manually list the full set of columns that uniquely identify a row.

Then, when using this in the relevant `Experiment` method, we need to list all these join keys again.

* Fix `Experiment.get_single_window_data()`

It appears that #116 / edc44eb2 broke `Experiment.get_single_window_data()` when replacing `run_query()` with `run_query_and_fetch()`. This was not caught by our tests because we can't mock BQ, so we can't test that method in a meaningful way (i.e. if we had a mock for run_query() we may well have forgotten to delete the mock, leaving the test still to pass).

src/mozanalysis/bq.py
@@ -36,11 +36,20 @@ class BigQueryContext:
         self.project_id = project_id
         self.client = bigquery.Client(project=project_id)
 
-    def run_query_and_fetch(self, sql, results_table=None):
+    def run_query(self, sql, results_table=None):
         """Run a query and return the result.
+
         If ``results_table`` is provided, then save the results
         into there (or just query from there if it already exists).
         Returns a ``google.cloud.bigquery.table.RowIterator``
+
+        Args:
+            sql (str): A SQL query.
+            results_table (str, optional): A table name, not including
+                a project_id or dataset_id. The table name is used as a
+                cache key (if the table already exists, we ignore ``sql``
+                and return the table's contents), so it is wise for
+                ``results_table`` to include a hash of ``sql``.
         """
         if not results_table:
             return self.client.query(sql).result()
@@ -58,12 +67,14 @@ class BigQueryContext:
             return full_res
 
         except Conflict:
-            print("Full results table already exists. Reusing", results_table)
-            return self.client.list_rows(results_table)
+            print("Table already exists. Reusing", results_table)
+            return self.client.list_rows(
+                self.fully_qualify_table_name(results_table)
+            )
 
     def fully_qualify_table_name(self, table_name):
         """Given a table name, return it fully qualified."""
-        return "`{project_id}.{dataset_id}.{full_table_name}`".format(
+        return "{project_id}.{dataset_id}.{full_table_name}".format(
             project_id=self.project_id,
             dataset_id=self.dataset_id,
             full_table_name=table_name,

src/mozanalysis/experiment.py
@@ -146,6 +146,10 @@ class Experiment:
 
                     WITH raw_enrollments AS ({custom_enrollments_query})
 
+                N.B. this query's results must be uniquely keyed by
+                (client_id, branch), or else your results will be subtly
+                wrong.
+
             segment_list (list of mozanalysis.segment.Segment): The user
                 segments to study.
 
@@ -191,19 +195,34 @@ class Experiment:
             segment_list=segment_list,
         )
 
-        enrollments_table = bq_context.run_query(enrollments_sql)
+        enrollments_table_name = sanitize_table_name_for_bq(
+            "_".join(
+                [
+                    last_date_full_data,
+                    "enrollments",
+                    self.experiment_slug,
+                    hash_ish(enrollments_sql),
+                ]
+            )
+        )
+
+        bq_context.run_query(
+            enrollments_sql, enrollments_table_name
+        )
 
         metrics_sql = self.build_metrics_query(
             metric_list=metric_list,
             time_limits=time_limits,
-            enrollments_table=enrollments_table,
+            enrollments_table=bq_context.fully_qualify_table_name(
+                enrollments_table_name
+            ),
         )
 
         full_res_table_name = sanitize_table_name_for_bq(
             "_".join([last_date_full_data, self.experiment_slug, hash_ish(metrics_sql)])
         )
 
-        return bq_context.run_query_and_fetch(
+        return bq_context.run_query(
             metrics_sql, full_res_table_name
         ).to_dataframe()
 
@@ -240,6 +259,10 @@ class Experiment:
                 in the main query::
 
                     WITH raw_enrollments AS ({custom_enrollments_query})
+
+                N.B. this query's results must be uniquely keyed by
+                (client_id, branch), or else your results will be subtly
+                wrong.
 
             segment_list (list of mozanalysis.segment.Segment): The user
                 segments to study.
@@ -294,21 +317,23 @@ class Experiment:
             )
         )
 
-        enrollments_table = bq_context.run_query_and_fetch(
+        bq_context.run_query(
             enrollments_sql, enrollments_table_name
         )
 
         metrics_sql = self.build_metrics_query(
             metric_list=metric_list,
             time_limits=time_limits,
-            enrollments_table=enrollments_table,
+            enrollments_table=bq_context.fully_qualify_table_name(
+                enrollments_table_name
+            ),
         )
 
         full_res_table_name = sanitize_table_name_for_bq(
             "_".join([last_date_full_data, self.experiment_slug, hash_ish(metrics_sql)])
         )
 
-        bq_context.run_query_and_fetch(metrics_sql, full_res_table_name)
+        bq_context.run_query(metrics_sql, full_res_table_name)
 
         return TimeSeriesResult(
             fully_qualified_table_name=bq_context.fully_qualify_table_name(
@@ -325,11 +350,6 @@ class Experiment:
         segment_list=None,
     ) -> str:
         """Return a SQL query for querying enrollments data.
-
-        For interactive use, prefer :meth:`.get_time_series_data` or
-        :meth:`.get_single_window_data`, according to your use case,
-        which will run the query for you and return a materialized
-        dataframe.
 
         Args:
             time_limits (TimeLimits): An object describing the
@@ -348,8 +368,6 @@ class Experiment:
 
         Returns:
             A string containing a BigQuery SQL expression.
-
-        Building this query is the main goal of this module.
         """
         analysis_windows_query = self._build_analysis_windows_query(
             time_limits.analysis_windows
@@ -369,9 +387,9 @@ class Experiment:
             segmented_enrollments AS ({segments_query})
 
             SELECT
-                e.*,
+                se.*,
                 aw.*
-            FROM segmented_enrollments e
+            FROM segmented_enrollments se
             CROSS JOIN analysis_windows aw
         """.format(
             analysis_windows_query=analysis_windows_query,
@@ -409,10 +427,14 @@ class Experiment:
         )
 
         return """
+        WITH enrollments AS (
+            SELECT *
+            FROM `{enrollments_table}`
+        )
         SELECT
-            {enrollments_table}.*,
+            enrollments.*,
             {metrics_columns}
-        FROM {enrollments_table}
+        FROM enrollments
         {metrics_joins}
         """.format(
             metrics_columns=",\n        ".join(metrics_columns),
@@ -527,7 +549,7 @@ class Experiment:
             metrics_joins.append(
                 """    LEFT JOIN (
         {query}
-        ) ds_{i} USING (client_id, analysis_window_start, analysis_window_end)
+        ) ds_{i} USING (client_id, branch, analysis_window_start, analysis_window_end)
                 """.format(
                     query=query_for_metrics, i=i
                 )
@@ -591,7 +613,7 @@ class Experiment:
             segments_joins.append(
                 """    LEFT JOIN (
         {query}
-        ) ds_{i} USING (client_id)
+        ) ds_{i} USING (client_id, branch)
                 """.format(
                     query=query_for_segments, i=i
                 )
@@ -880,7 +902,7 @@ class TimeSeriesResult:
                     "AnalysisWindow not found with start of {}".format(analysis_window)
                 )
 
-        return bq_context.run_query_and_fetch(
+        return bq_context.run_query(
             self._build_analysis_window_subset_query(analysis_window)
         ).to_dataframe()
 

src/mozanalysis/metrics/__init__.py
@@ -128,6 +128,7 @@ class DataSource:
         """
         return """SELECT
             e.client_id,
+            e.branch,
             e.analysis_window_start,
             e.analysis_window_end,
             {metrics}
@@ -139,7 +140,11 @@ class DataSource:
                     DATE_ADD(e.enrollment_date, interval e.analysis_window_start day)
                     AND DATE_ADD(e.enrollment_date, interval e.analysis_window_end day)
                 {ignore_pre_enroll_first_day}
-        GROUP BY e.client_id, e.analysis_window_start, e.analysis_window_end""".format(
+        GROUP BY
+            e.client_id,
+            e.branch,
+            e.analysis_window_start,
+            e.analysis_window_end""".format(
             client_id=self.client_id_column,
             submission_date=self.submission_date_column,
             from_expr=self.from_expr_for(from_expr_dataset),

src/mozanalysis/segments/__init__.py
@@ -87,6 +87,7 @@ class SegmentDataSource:
         """
         return """SELECT
             e.client_id,
+            e.branch,
             {segments}
         FROM raw_enrollments e
             LEFT JOIN {from_expr} ds
@@ -97,7 +98,7 @@ class SegmentDataSource:
                 AND ds.{submission_date} BETWEEN
                     DATE_ADD(e.enrollment_date, interval {window_start} day)
                     AND DATE_ADD(e.enrollment_date, interval {window_end} day)
-        GROUP BY e.client_id""".format(
+        GROUP BY e.client_id, e.branch""".format(
             client_id=self.client_id_column,
             submission_date=self.submission_date_column,
             from_expr=self.from_expr_for(from_expr_dataset),