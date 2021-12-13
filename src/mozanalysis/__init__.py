4c044c6000ea78974fc693d1adf6735924d9894b
kik-kik <42538694+kik-kik@users.noreply.github.com>
2021-12-13 12:00:35 +0100

    mozanalysis init comment updated

--- src/mozanalysis/__init__.py
+++ src/mozanalysis/__init__.py
@@ -2,8 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# This allows wildcard imports: from mozanalysis.metrics import *
-# Generally, import * should be avoided, however it is needed by Jetstream
+# This allows wildcard imports: from mozanalysis import *
+# Generally, import * should be avoided, however it is used by Jetstream
+# We use it to expose modules available in mozanalysis package.
 # https://github.com/mozilla/jetstream
 import os
 
