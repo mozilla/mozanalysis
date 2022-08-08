954e74a363484ad601d3f7517e53122d4266d103
Anna Scholtz <anna@scholtzan.net>
2022-08-08 17:01:09 +0200

    Update docs

--- src/mozanalysis/metrics/desktop.py
+++ src/mozanalysis/metrics/desktop.py
@@ -2,6 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# These metric definitions are deprected.
+# Instead use the metric slug to reference metrics defined
+# in https://github.com/mozilla/jetstream-config
+
 from mozanalysis.config import ConfigLoader
 
 clients_daily = ConfigLoader.get_data_source("clients_daily", "firefox_desktop")
