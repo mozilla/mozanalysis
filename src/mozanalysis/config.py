7936cb039a5b87c2dee3fa04d44be954b3cb8399
Anna Scholtz <anna@scholtzan.net>
2022-08-08 13:45:01 +0200

    Load definitions from external config repository

--- src/mozanalysis/config.py
+++ src/mozanalysis/config.py
@@ -2,15 +2,22 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from asyncio import windows_events
 from typing import Optional
-import attr
-from pytest import Config
 
+import attr
 from jetstream_config_parser.config import ConfigCollection
 
 from mozanalysis.metrics import DataSource, Metric
+from mozanalysis.segments import Segment, SegmentDataSource
+
 
 class _ConfigLoader:
+    """
+    Loads config files from an external repository.
+
+    Config objects are converted into mozanalysis native types.
+    """
     config_collection: Optional[ConfigCollection] = attr.ib(None)
 
     @property
@@ -33,7 +40,7 @@ class _ConfigLoader:
             select_expr=self.configs.get_env().from_string(metric_definition.select_expression).render(),
             friendly_name=metric_definition.friendly_name,
             description=metric_definition.friendly_name,
-            data_source=self.configs.get_data_source(metric_definition.data_source.name, app_name),
+            data_source=self.get_data_source(metric_definition.data_source.name, app_name),
             bigger_is_better=metric_definition.bigger_is_better
         )
 
@@ -51,7 +58,32 @@ class _ConfigLoader:
             default_dataset=data_source_definition.default_dataset
         )
 
-    def get_segment():
-        pass
+    def get_segment(self, segment_slug: str, app_name: str):
+        segment_definition = self.configs.get_segment_definition(segment_slug, app_name)
+        if segment_definition is None:
+            raise Exception(f"Could not find definition for segment {segment_slug}")
+
+        return Segment(
+            name=segment_definition.name,
+            data_source=self.get_segment_data_source(segment_definition.data_source.name, app_name),
+            select_expr=segment_definition.select_expression,
+            friendly_name=segment_definition.friendly_name,
+            description=segment_definition.description
+        )
+
+    def get_segment_data_source(self, data_source_slug: str, app_name: str):
+        data_source_definition = self.configs.get_segment_data_source_definition(data_source_slug, app_name)
+        if data_source_definition is None:
+            raise Exception(f"Could not find definition for segment data source {data_source_slug}")
+
+        return SegmentDataSource(
+            name=data_source_definition.name,
+            from_expr=data_source_definition.from_expression,
+            window_start=data_source_definition.window_start,
+            windows_end=data_source_definition.window_end,
+            client_id_column=data_source_definition.client_id_column,
+            submission_date_column=data_source_definition.submission_date_column,
+            default_dataset=data_source_definition.default_dataset
+        )
 
 ConfigLoader = _ConfigLoader()
--- src/mozanalysis/metrics/klar_android.py
+++ src/mozanalysis/metrics/klar_android.py
@@ -2,72 +2,17 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from mozanalysis.metrics import DataSource, Metric
+from mozanalysis.config import ConfigLoader
 
-baseline = DataSource(
-    name="baseline",
-    from_expr="""(
-                SELECT
-                    p.*,
-                    DATE(p.submission_timestamp) AS submission_date
-                FROM `moz-fx-data-shared-prod.{dataset}.baseline` p
-            )""",
-    client_id_column="client_info.client_id",
-    experiments_column_type="glean",
-    default_dataset="org_mozilla_klar",
-)
+baseline = ConfigLoader.get_data_source("baseline", "klar_android")
+
+events = ConfigLoader.get_data_source("events", "klar_android")
+
+metrics = ConfigLoader.get_data_source("metrics", "klar_android")
 
 
-events = DataSource(
-    name="events",
-    from_expr="""(
-                SELECT
-                    p.* EXCEPT (events),
-                    DATE(p.submission_timestamp) AS submission_date,
-                    event
-                FROM
-                    `moz-fx-data-shared-prod.{dataset}.events` p
-                CROSS JOIN
-                    UNNEST(p.events) AS event
-            )""",
-    client_id_column="client_info.client_id",
-    experiments_column_type="glean",
-    default_dataset="org_mozilla_klar",
-)
+baseline_ping_count = ConfigLoader.get_metric("baseline_ping_count", "klar_android")
 
-metrics = DataSource(
-    name="metrics",
-    from_expr="""(
-                SELECT
-                    p.*,
-                    DATE(p.submission_timestamp) AS submission_date
-                FROM `moz-fx-data-shared-prod.{dataset}.metrics` p
-            )""",
-    client_id_column="client_info.client_id",
-    experiments_column_type="glean",
-    default_dataset="org_mozilla_klar",
-)
+metric_ping_count = ConfigLoader.get_metric("metric_ping_count", "klar_android")
 
-baseline_ping_count = Metric(
-    name="baseline_ping_count",
-    data_source=baseline,
-    select_expr="COUNT(document_id)",
-    friendly_name="Baseline pings",
-    description="Counts the number of `baseline` pings received from each client.",
-)
-
-metric_ping_count = Metric(
-    name="metric_ping_count",
-    data_source=metrics,
-    select_expr="COUNT(document_id)",
-    friendly_name="Metrics pings",
-    description="Counts the number of `metrics` pings received from each client.",
-)
-
-first_run_date = Metric(
-    name="first_run_date",
-    data_source=baseline,
-    select_expr="MIN(client_info.first_run_date)",
-    friendly_name="First run date",
-    description="The earliest first-run date reported by each client.",
-)
+first_run_date = ConfigLoader.get_metric("first_run_date", "klar_android")
