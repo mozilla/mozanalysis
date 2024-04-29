d69a529a6ba9df96f932df206308ed8a1f061bac
Daniel Berry <dberry@mozilla.com>
2024-04-29 08:08:44 -0700

    updates

--- src/mozanalysis/visualization/statistics/empirical_cdf.py
+++ src/mozanalysis/visualization/statistics/empirical_cdf.py
@@ -8,49 +8,52 @@ from mozanalysis.visualization.plotters import (
 )
 from mozanalysis.visualization.StatisticsData import StatisticType
 from mozanalysis.visualization.PlotType import PlotType
+import matplotlib.pyplot as plt
+import pandas as pd
 
-onetime_empricalcdf_individual = dedent(
-    f"""\
-        def onetime_empricalcdf_individual(
-                period_df: pd.DataFrame,
-                metric: str, 
-                analysis_period:str,
-                analysis_basis:str,
-                reference_branch:str,
-                branches:List[str],
-            ) -> None:
 
-            fig, ax = plt.subplots()
-            individual_df = period_df.loc[(
-                (period_df.metric == metric) &
-                (period_df.statistic == 'empirical_cdf') &
-                (period_df.analysis_basis == analysis_basis) & 
-                (period_df.comparison.isna())
-            )]
+def onetime_empricalcdf_individual(
+    period_df: pd.DataFrame,
+    metric: str,
+    analysis_period: str,
+    analysis_basis: str,
+    reference_branch: str,
+    branches: List[str],
+) -> None:
 
-            observed_parameters = set()
-            for branch in branches:
-                branch_df = individual_df.loc[(individual_df.branch == branch)]
-                plt.plot(branch_df.parameter, branch_df.point)
-                plt.fill_between(
-                    branch_df.parameter, 
-                    branch_df.lower,
-                    branch_df.upper,
-                    alpha = 0.2, 
-                    label = branch
-                )
-                for parameter in branch_df.parameter:
-                    observed_parameters.add(parameter)
-            plt.xticks(sorted(list(observed_parameters)))
-            plt.legend()
-            plt.xlabel(analysis_period)
-            
-            plt.ylabel(metric)
-            plt.show()"""
-)
+    fig, ax = plt.subplots()
+    individual_df = period_df.loc[
+        (
+            (period_df.metric == metric)
+            & (period_df.statistic == "empirical_cdf")
+            & (period_df.analysis_basis == analysis_basis)
+            & (period_df.comparison.isna())
+        )
+    ]
 
-for plotter in (onetime_empricalcdf_individual,):
-    PlotterRegistry.register_plotter(plotter)
+    observed_parameters = set()
+    for branch in branches:
+        branch_df = individual_df.loc[(individual_df.branch == branch)]
+        plt.plot(branch_df.parameter, branch_df.point)
+        plt.fill_between(
+            branch_df.parameter,
+            branch_df.lower,
+            branch_df.upper,
+            alpha=0.2,
+            label=branch,
+        )
+        for parameter in branch_df.parameter:
+            observed_parameters.add(parameter)
+    plt.xticks(sorted(list(observed_parameters)))
+    plt.legend()
+    plt.xlabel(analysis_period)
+
+    plt.ylabel(metric)
+    plt.show()
+
+
+# for plotter in (onetime_empricalcdf_individual,):
+#     PlotterRegistry.register_plotter(plotter)
 
 
 def dispatch_empiricalcdf_onetime(call_plotter_params: List[Any]) -> List[NotebookNode]:
