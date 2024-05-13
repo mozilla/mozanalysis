e63c82cc2d0fd7f6307e3100cee9dcee7d0265c2
Daniel Berry <dberry@mozilla.com>
2024-05-13 09:29:29 -0700

    linting

--- tests/frequentist_stats/test_linear_models.py
+++ tests/frequentist_stats/test_linear_models.py
@@ -281,58 +281,78 @@ def test_summarize_joint():
 
 def test__make_model_df():
     # test removal of nulls
-    y = [1,2,3,4,None, np.nan]
+    y = [1, 2, 3, 4, None, np.nan]
     branch = ["control", "control", "treatment", "treatment", "control", "treatment"]
-    df_in = pd.DataFrame({"y":y, "branch":branch})
+    df_in = pd.DataFrame({"y": y, "branch": branch})
     df_actual = mafslm._make_model_df(df_in, "y")
-    df_expected = pd.DataFrame({"branch":branch[:4],"y":[1.0,2.0,3.0,4.0]})
+    df_expected = pd.DataFrame({"branch": branch[:4], "y": [1.0, 2.0, 3.0, 4.0]})
     pd.testing.assert_frame_equal(df_actual, df_expected)
 
     # test removal of nulls with covariate
-    y = [1,2,3,4,None, np.nan]
-    y2 = [1,2, None, np.nan,3,4]
+    y = [1, 2, 3, 4, None, np.nan]
+    y2 = [1, 2, None, np.nan, 3, 4]
     branch = ["control", "control", "treatment", "treatment", "control", "treatment"]
-    df_in = pd.DataFrame({"y":y, "branch":branch, "y2":y2})
-    df_actual = mafslm._make_model_df(df_in, "y", covariate_col_label ="y2")
-    df_expected = pd.DataFrame({"branch":branch[:2],"y":[1.0,2.0], "y2":[1.0, 2.0]})
+    df_in = pd.DataFrame({"y": y, "branch": branch, "y2": y2})
+    df_actual = mafslm._make_model_df(df_in, "y", covariate_col_label="y2")
+    df_expected = pd.DataFrame(
+        {"branch": branch[:2], "y": [1.0, 2.0], "y2": [1.0, 2.0]}
+    )
     pd.testing.assert_frame_equal(df_actual, df_expected)
 
     # test thresholding
     y = list(range(100))
-    branch = ["control"]*100
-    df_in = pd.DataFrame({"y":y, "branch":branch})
-    df_actual = mafslm._make_model_df(df_in, "y", threshold_quantile = 0.95)
-    df_expected = pd.DataFrame({"branch":branch, "y":[float(_y) for _y in y[:-5] + [np.quantile(y, 0.95)]*5]})
+    branch = ["control"] * 100
+    df_in = pd.DataFrame({"y": y, "branch": branch})
+    df_actual = mafslm._make_model_df(df_in, "y", threshold_quantile=0.95)
+    df_expected = pd.DataFrame(
+        {
+            "branch": branch,
+            "y": [float(_y) for _y in y[:-5] + [np.quantile(y, 0.95)] * 5],
+        }
+    )
     pd.testing.assert_frame_equal(df_actual, df_expected)
 
     # test covariate & thresholding
-    y2 = list(range(100,200))
-    branch = ["control"]*100
-    df_in = pd.DataFrame({"y":y, "branch":branch, "y2":y2})
-    df_actual = mafslm._make_model_df(df_in, "y", covariate_col_label="y2", threshold_quantile = 0.95)
-    df_expected = pd.DataFrame({
-        "branch":branch,
-        "y":[float(_y) for _y in y[:-5] + [np.quantile(y, 0.95)]*5],
-        "y2":[float(_y) for _y in y2[:-5]+[np.quantile(y2, 0.95)]*5]
-    })
+    y2 = list(range(100, 200))
+    branch = ["control"] * 100
+    df_in = pd.DataFrame({"y": y, "branch": branch, "y2": y2})
+    df_actual = mafslm._make_model_df(
+        df_in, "y", covariate_col_label="y2", threshold_quantile=0.95
+    )
+    df_expected = pd.DataFrame(
+        {
+            "branch": branch,
+            "y": [float(_y) for _y in y[:-5] + [np.quantile(y, 0.95)] * 5],
+            "y2": [float(_y) for _y in y2[:-5] + [np.quantile(y2, 0.95)] * 5],
+        }
+    )
     pd.testing.assert_frame_equal(df_actual, df_expected)
 
 
 def test_compare_branches_lm():
-    """This is an integration type test, testing only that the 
-    format of the output object is correct. Functionality testing 
+    """This is an integration type test, testing only that the
+    format of the output object is correct. Functionality testing
     of specific elements of the object is covered by other tests"""
     branches = ["control", "treatment-a", "treatment-b"]
     _, alphas, _, model_df, column_label = _make_test_model()
 
-    out = mafslm.compare_branches_lm(model_df, column_label, "treatment-a", alphas=alphas)
+    out = mafslm.compare_branches_lm(
+        model_df, column_label, "treatment-a", alphas=alphas
+    )
 
     assert list(out.keys()) == ["individual", "comparative"]
 
     # assert sorted(list(out['individual'].keys())) == branches
 
     for branch in branches:
-        assert sorted(out["individual"][branch].index) == ["0.005", "0.025", "0.5", "0.975", "0.995", "mean"]
+        assert sorted(out["individual"][branch].index) == [
+            "0.005",
+            "0.025",
+            "0.5",
+            "0.975",
+            "0.995",
+            "mean",
+        ]
 
     comparative_branches = ["control", "treatment-b"]
 
