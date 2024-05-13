a7bb4400739d678953deeb27e39c29e86c3fd470
Daniel Berry <dberry@mozilla.com>
2024-05-13 15:31:55 +0000

    more test cases

--- tests/frequentist_stats/test_linear_models.py
+++ tests/frequentist_stats/test_linear_models.py
@@ -1,3 +1,4 @@
+import mozanalysis.frequentist_stats.bootstrap as mafsb
 import mozanalysis.frequentist_stats.linear_models as mafslm
 import numpy as np
 import pandas as pd
@@ -15,7 +16,7 @@ def _make_test_model():
     formula = mafslm._make_formula("search_count", ref_branch)
     results = smf.ols(formula, model_df).fit()
 
-    return results, alphas, ref_branch, model_df
+    return results, alphas, ref_branch, model_df, "search_count"
 
 
 def test_stringify_alpha():
@@ -67,9 +68,9 @@ def test_summarize_univariate():
 
     assert sorted(result.keys()) == branch_list
 
+    # validate against theoretical values
     mean = 49.5
     low, high = 43.74349, 55.25650
-
     for branch in branch_list:
         assert np.isclose(result[branch]["exp"], mean)
         assert np.isclose(result[branch]["0.5"], mean)
@@ -79,6 +80,14 @@ def test_summarize_univariate():
         index_values = result[branch].index.values
         index_values.sort()
         assert list(index_values) == ["0.025", "0.5", "0.975", "exp"]
+
+    # cross-validate against existing bootstrap implementation
+    bootstrap_result = mafsb.bootstrap_one_branch(one_branch_data)
+    for branch in branch_list:
+        assert np.isclose(result[branch]["exp"], bootstrap_result["mean"], atol=0.5)
+        assert np.isclose(result[branch]["0.5"], bootstrap_result["mean"], atol=0.5)
+        assert np.isclose(result[branch]["0.025"], bootstrap_result["0.025"], atol=0.5)
+        assert np.isclose(result[branch]["0.975"], bootstrap_result["0.975"], atol=0.5)
 
 
 def test__make_formula():
@@ -120,34 +129,84 @@ def test__make_joint_output():
 
 
 def test__extract_absolute_uplifts():
-    results, alphas, ref_branch, model_df = _make_test_model()
+    results, alphas, ref_branch, model_df, column_label = _make_test_model()
+
+    bootstrap_results = mafsb.compare_branches(model_df, column_label, ref_branch_label=ref_branch)
 
     # control vs treatment-a
     out = mafslm._extract_absolute_uplifts(results, "control", ref_branch, alphas)
 
-    assert np.isclose(out[("abs_uplift", "exp")], -100)
-    assert np.isclose(out[("abs_uplift", "0.5")], -100)
+    expected_mean = 50-150
+    assert np.isclose(out[("abs_uplift", "exp")], expected_mean)
+    assert np.isclose(out[("abs_uplift", "0.5")], expected_mean)
 
     for alpha in alphas:
         a_str_low, a_str_high = mafslm.stringify_alpha(alpha)
         low, high = CompareMeans.from_data(
-            model_df.loc[model_df.branch == "control", "search_count"],
-            model_df.loc[model_df.branch == ref_branch, "search_count"],
+            model_df.loc[model_df.branch == "control", column_label],
+            model_df.loc[model_df.branch == ref_branch, column_label],
         ).zconfint_diff(alpha)
         assert np.isclose(out[("abs_uplift", a_str_low)], low, atol=0.1)
         assert np.isclose(out[("abs_uplift", a_str_high)], high, atol=0.1)
 
+    ## cross-validate with existing bootstrap implementation
+    boot_df = bootstrap_results["comparative"]["control"]
+    for alpha in alphas:
+        a_str_low, a_str_high = mafslm.stringify_alpha(alpha)
+        assert np.isclose(out[("abs_uplift", a_str_low)], boot_df[("abs_uplift", a_str_low)], atol=1.0)
+        assert np.isclose(out[("abs_uplift", a_str_high)], boot_df[("abs_uplift", a_str_high)], atol=1.0)
     # treatment-a vs treatment-b
     out = mafslm._extract_absolute_uplifts(results, "treatment-b", ref_branch, alphas)
 
-    assert np.isclose(out[("abs_uplift", "exp")], 100)
-    assert np.isclose(out[("abs_uplift", "0.5")], 100)
+    expected_mean = 250-150
+    assert np.isclose(out[("abs_uplift", "exp")], expected_mean)
+    assert np.isclose(out[("abs_uplift", "0.5")], expected_mean)
 
     for alpha in alphas:
         a_str_low, a_str_high = mafslm.stringify_alpha(alpha)
         low, high = CompareMeans.from_data(
-            model_df.loc[model_df.branch == "treatment-b", "search_count"],
-            model_df.loc[model_df.branch == ref_branch, "search_count"],
+            model_df.loc[model_df.branch == "treatment-b", column_label],
+            model_df.loc[model_df.branch == ref_branch, column_label],
         ).zconfint_diff(alpha)
         assert np.isclose(out[("abs_uplift", a_str_low)], low, atol=0.1)
         assert np.isclose(out[("abs_uplift", a_str_high)], high, atol=0.1)
+
+    ## cross-validate with existing bootstrap implementation
+    boot_df = bootstrap_results["comparative"]["treatment-b"]
+    for alpha in alphas:
+        a_str_low, a_str_high = mafslm.stringify_alpha(alpha)
+        assert np.isclose(out[("abs_uplift", a_str_low)], boot_df[("abs_uplift", a_str_low)], atol=1.0)
+        assert np.isclose(out[("abs_uplift", a_str_high)], boot_df[("abs_uplift", a_str_high)], atol=1.0)
+
+def test__extract_relative_uplifts():
+    results, alphas, ref_branch, model_df, column_label = _make_test_model()
+
+    bootstrap_results = mafsb.compare_branches(model_df, column_label, ref_branch_label=ref_branch)
+
+    # control vs treatment-a
+    out = mafslm._extract_relative_uplifts(results, "control", ref_branch, alphas)
+
+    expected_mean = (50-150)/150
+    assert np.isclose(out[("rel_uplift", "exp")], expected_mean, atol=0.01)
+    assert np.isclose(out[("rel_uplift", "0.5")], expected_mean, atol=0.01)
+
+    ## cross-validate with existing bootstrap implementation
+    boot_df = bootstrap_results["comparative"]["control"]
+    for alpha in alphas:
+        a_str_low, a_str_high = mafslm.stringify_alpha(alpha)
+        assert np.isclose(out[("rel_uplift", a_str_low)], boot_df[("rel_uplift", a_str_low)], atol=0.01)
+        assert np.isclose(out[("rel_uplift", a_str_high)], boot_df[("rel_uplift", a_str_high)], atol=0.01)
+
+
+    out = mafslm._extract_relative_uplifts(results, "treatment-b", ref_branch, alphas)
+
+    expected_mean = (250-150)/150
+    assert np.isclose(out[("rel_uplift", "exp")], expected_mean, atol=0.01)
+    assert np.isclose(out[("rel_uplift", "0.5")], expected_mean, atol=0.01)
+
+    ## cross-validate with existing bootstrap implementation
+    boot_df = bootstrap_results["comparative"]["treatment-b"]
+    for alpha in alphas:
+        a_str_low, a_str_high = mafslm.stringify_alpha(alpha)
+        assert np.isclose(out[("rel_uplift", a_str_low)], boot_df[("rel_uplift", a_str_low)], atol=0.01)
+        assert np.isclose(out[("rel_uplift", a_str_high)], boot_df[("rel_uplift", a_str_high)], atol=0.01)
