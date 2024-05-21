78ac6e824ec9797f9b77f13e9180213d01fdd26f
Jared Snyder <jsnyder@mozilla.com>
2024-05-21 15:06:34 -0500

    actually add stuff to the file I created instead of being a moron

--- tests/helpers/config_loader_lists.py
+++ tests/helpers/config_loader_lists.py
@@ -7,3 +7,24 @@ desktop_segments = [
     ConfigLoader.get_segment("allweek_regular_v1", "firefox_desktop"),
     ConfigLoader.get_segment("new_unique_profiles", "firefox_desktop"),
 ]
+
+desktop_metrics = [
+    ConfigLoader.get_metric("active_hours", "firefox_desktop"),
+    ConfigLoader.get_metric("uri_count", "firefox_desktop"),
+    ConfigLoader.get_metric("search_count", "firefox_desktop"),
+    ConfigLoader.get_metric("tagged_search_count", "firefox_desktop"),
+    ConfigLoader.get_metric("tagged_follow_on_search_count", "firefox_desktop"),
+    ConfigLoader.get_metric("ad_clicks", "firefox_desktop"),
+    ConfigLoader.get_metric("searches_with_ads", "firefox_desktop"),
+    ConfigLoader.get_metric("organic_search_count", "firefox_desktop"),
+    ConfigLoader.get_metric("unenroll", "firefox_desktop"),
+    ConfigLoader.get_metric("view_about_logins", "firefox_desktop"),
+    ConfigLoader.get_metric("view_about_protections", "firefox_desktop"),
+    ConfigLoader.get_metric("connect_fxa", "firefox_desktop"),
+    ConfigLoader.get_metric("pocket_rec_clicks", "firefox_desktop"),
+    ConfigLoader.get_metric("pocket_spoc_clicks", "firefox_desktop"),
+    ConfigLoader.get_metric("days_of_use", "firefox_desktop"),
+    ConfigLoader.get_metric("qualified_cumulative_days_of_use", "firefox_desktop"),
+    ConfigLoader.get_metric("disable_pocket_clicks", "firefox_desktop"),
+    ConfigLoader.get_metric("disable_pocket_spocs_clicks", "firefox_desktop"),
+]
