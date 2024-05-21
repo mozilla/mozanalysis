f178b857af12781aa61e653eec327bcf0e5b87a5
Jared Snyder <jsnyder@mozilla.com>
2024-05-21 15:13:21 -0500

    import lists in test

--- tests/test_sizing.py
+++ tests/test_sizing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pytest
-from helpers.cheap_lint import sql_lint
+from helpers.cheap_lint import sql_lint  # local helper file
+from helpers.config_loader_lists import desktop_metrics, desktop_segments
 from mozanalysis.config import ConfigLoader
 from mozanalysis.experiment import TimeLimits
 from mozanalysis.metrics import DataSource, Metric
@@ -185,41 +186,12 @@ def test_megaquery_not_detectably_malformed():
         analysis_length_dates=3,
     )
 
-    desktop_segments = [
-        ConfigLoader.get_segment("regular_users_v3", "firefox_desktop"),
-        ConfigLoader.get_segment("new_or_resurrected_v3", "firefox_desktop"),
-        ConfigLoader.get_segment("weekday_regular_v1", "firefox_desktop"),
-        ConfigLoader.get_segment("allweek_regular_v1", "firefox_desktop"),
-        ConfigLoader.get_segment("new_unique_profiles", "firefox_desktop"),
-    ]
-
     target_sql = test_target.build_targets_query(
         time_limits=tl,
         target_list=desktop_segments,
     )
 
     sql_lint(target_sql)
-
-    desktop_metrics = [
-        ConfigLoader.get_metric("active_hours", "firefox_desktop"),
-        ConfigLoader.get_metric("uri_count", "firefox_desktop"),
-        ConfigLoader.get_metric("search_count", "firefox_desktop"),
-        ConfigLoader.get_metric("tagged_search_count", "firefox_desktop"),
-        ConfigLoader.get_metric("tagged_follow_on_search_count", "firefox_desktop"),
-        ConfigLoader.get_metric("ad_clicks", "firefox_desktop"),
-        ConfigLoader.get_metric("searches_with_ads", "firefox_desktop"),
-        ConfigLoader.get_metric("organic_search_count", "firefox_desktop"),
-        ConfigLoader.get_metric("unenroll", "firefox_desktop"),
-        ConfigLoader.get_metric("view_about_logins", "firefox_desktop"),
-        ConfigLoader.get_metric("view_about_protections", "firefox_desktop"),
-        ConfigLoader.get_metric("connect_fxa", "firefox_desktop"),
-        ConfigLoader.get_metric("pocket_rec_clicks", "firefox_desktop"),
-        ConfigLoader.get_metric("pocket_spoc_clicks", "firefox_desktop"),
-        ConfigLoader.get_metric("days_of_use", "firefox_desktop"),
-        ConfigLoader.get_metric("qualified_cumulative_days_of_use", "firefox_desktop"),
-        ConfigLoader.get_metric("disable_pocket_clicks", "firefox_desktop"),
-        ConfigLoader.get_metric("disable_pocket_spocs_clicks", "firefox_desktop"),
-    ]
 
     desktop_metrics_no_experiment_slug = [
         m for m in desktop_metrics if "experiment_slug" not in m.select_expr
