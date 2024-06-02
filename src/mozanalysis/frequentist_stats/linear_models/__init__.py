5fecd3feebe21832d6d3b69e171c456279f078e8
Daniel Berry <dberry@mozilla.com>
2024-06-02 14:26:09 -0700

    cleanup

---
+++ src/mozanalysis/frequentist_stats/linear_models/__init__.py
@@ -0,0 +1,9 @@
+from .classes import MozOLS  # noqa: F401
+from .functions import (
+    compare_branches_lm,  # noqa: F401
+    fit_model,  # noqa: F401
+    make_formula,  # noqa: F401
+    summarize_joint,  # noqa: F401
+    summarize_one_branch,  # noqa: F401
+    summarize_univariate,  # noqa: F401
+)
