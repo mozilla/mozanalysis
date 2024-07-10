84d7e1594a30dbf5c75d5ea9b9b1eae38dd4f68a
Daniel Berry <dberry@mozilla.com>
2024-07-10 14:59:54 -0700

    forgot types file

---
+++ src/mozanalysis/types.py
@@ -0,0 +1,21 @@
+from enum import Enum
+from typing import TypeAlias
+
+import pandas as pd
+
+
+class ComparativeOption(str, Enum):
+    INDIVIDUAL = "individual"
+    COMPARATIVE = "comparative"
+
+
+class Uplift(str, Enum):
+    ABSOLUTE = "abs_uplift"
+    RELATIVE = "rel_uplift"
+
+
+Estimates: TypeAlias = pd.Series
+BranchLabel = str
+EstimatesByBranch = dict[BranchLabel, Estimates]
+
+CompareBranchesOutput = dict[ComparativeOption, EstimatesByBranch]
